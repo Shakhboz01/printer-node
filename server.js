808bb56d41023c7a1cefc6892f8c013ed535ead4
Shakhboz <shfazliddinov1940@gmail.com>
2024-07-12 08:45:08 +0500

    m

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
+// https://zadig.akeo.ie/
+// comment out usb.on from node_modules/escpos-usb/index.js:52-59
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const escpos = require('escpos');
-escpos.Network = require('escpos-network');
+escpos.USB = require('escpos-usb');
+const usb = require('usb');
 
 const app = express();
 const port = 4000; // or any port of your choice
 app.use(bodyParser.json());
+
+const companyName = 'SMS CLINIC'
+const companyAddress = 'Fisdavsiy'
+const companyPhoneNumber = '97 111 11 11'
 const originUrl = 'https://web-production-80fc3.up.railway.app';
-const printer_ip_address = '192.168.123.100'
-const printer_port = '9100'
+const footerText = 'Спасибо за покупку'
+
+function formatter(num) {
+  return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 ');
+}
 
 app.get('/print/:sale_id', async (req, response) => {
   const sale_id = req.params.sale_id;
@@ -19,11 +30,8 @@ app.get('/print/:sale_id', async (req, response) => {
     const apiResponse = await axios.get(url);
     const saleData = apiResponse.data;
 
-    // Print receipt
     printReceipt(saleData);
-
-    // Send the sale data back as the response
-    response.send(saleData);
+    response.redirect(`${originUrl}/sales`);
   } catch (error) {
     console.error(`Error fetching sale with ID ${sale_id}:`, error);
     response.status(500).send('Error fetching sale information');
@@ -32,53 +40,69 @@ app.get('/print/:sale_id', async (req, response) => {
 
 // Function to print receipt based on sale data
 function printReceipt(saleData) {
-  // Create a new printer
-  const printer = new escpos.Network(printer_ip_address, printer_port);
+  const device = new escpos.USB();
+  const options = { encoding: "Win1251" };
+  const printer = new escpos.Printer(device, options);
 
-  // Create a new buffer for the receipt content
-  const buffer = new escpos.Screen();
+  device.open(function(error) {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    printer.font('a');
+    printer.align('ct');
+    printer.style('bu');
+    printer.size(1, 1)
+           .text(`${companyName}`)
+           .text('');
+    printer.size(0.5, 0.8);
+    printer.tableCustom(
+      [
+        { text:`${new Date(saleData.created_at * 1000).toLocaleString()}`, align:"LEFT", width:0.5 },
+        { text: `# ${saleData.id}`, align:"RIGHT", width:0.5 }
+      ]
+    )
+    printer.align('lt');
+    printer
+    .text(`Клиент: ${saleData.buyer_name}`)
+    .text(`Кассир: ${saleData.registrator}`);
 
-  // Add sale details to the receipt
-  buffer
-    .text(`Mijoz: ${saleData.buyer_name.capitalize()}`)
-    .text(`Vaqt: ${new Date(saleData.created_at * 1000).toLocaleString()}`)
-    .text(`ID: ${saleData.id}`)
-    .text(`SMS CLINIC`)
-    .text('-----------------------------------------------');
+    printer.align('ct');
+    printer.size(0.5, 0.7);
+    printer.drawLine();
+    printer.size(0.5, 0.5);
+    // Add product sells to the receipt
+    saleData.product_sells.forEach((product, index) => {
+      const total_price = product.amount * product.sell_price;
+      printer.tableCustom(
+        [
+          { text:`${index + 1}-${product.product_name}`, align:"LEFT", width: 0.37 },
+          { text:`${product.amount} * ${formatter(product.sell_price)}`, align:"CENTER", width: 0.38 },
+          { text: `${formatter(total_price)}`, align:"RIGHT", width:0.25 }
+        ]
+      )
+    });
+    printer.drawLine();
+    printer.size(0.5, 0.5);
+    printer.align('RT');
+    printer
+      .text(`Итого: ${formatter(saleData.total_price)}`)
+      .text(`${saleData.comment}`)
+      .drawLine();
 
-  // Add product sells to the receipt
-  saleData.product_sells.forEach((product, index) => {
-    const total_price = product.amount * product.sell_price;
-    buffer
-      .text(`${index + 1}. ${product.product_name}`)
-      .text(`${product.amount} * ${product.sell_price} = ${total_price}`);
-  });
-
-  buffer
-    .text('-----------------------------------------------')
-    .text(`Jami: ${saleData.total_price}`)
-    .text(`${saleData.comment}`)
-    .text('-----------------------------------------------')
-    .text(`93 414 00 30`)
-    .text(`Address: Andijon shahar Shaxid tepa 1-uy`)
-    .text('-----------------------------------------------')
-    .text('') // Add some empty lines for spacing
-    .text('')
-    .text('')
-    .text('')
-    .text('')
-    .text('')
-    .text('')
-    .text('')
-    .text('');
-
-  // Connect to the printer
-  printer.open();
-
-  // Send the receipt to the printer
-  printer
-    .send(buffer)
-    .close();
+    printer
+      .align('ct')
+      .text(companyPhoneNumber)
+      .text(`Адрес: ${companyAddress}`)
+      .text('')
+      .text(footerText)
+      .text('')
+      .text('')
+      .text('')
+      .text('')
+    printer.cut();
+    printer.close();
+  })
 }
 
 app.listen(port, () => {
